cfe3c5208d3dd6b9697b6219d1889ac5973cce59
yamanidev <elamine.yamani@gmail.com>
2024-11-18 00:17:52 +0100

    applied max width and horizontal centering on container

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import PublicLayout from "@/components/layout/PublicLayout";
 export default function Home() {
   return (
     <PublicLayout>
-      <h1>Liftoon</h1>
+      <div className="mx-auto w-full max-w-7xl px-2">
+        <h1>Liftoon</h1>
+      </div>
     </PublicLayout>
   );
 }
