9c81189501d632f8d0a908e4a2d29f08749af877
yamanidev <elamine.yamani@gmail.com>
2024-11-18 05:34:15 +0100

    added visual editing feature

--- sanity.config.ts
+++ sanity.config.ts
@@ -12,6 +12,7 @@ import { structureTool } from "sanity/structure";
 import { apiVersion, dataset, projectId } from "./sanity/env";
 import { schema } from "./sanity/schemaTypes";
 import { structure } from "./sanity/structure";
+import { presentationTool } from "sanity/presentation";
 
 export default defineConfig({
   basePath: "/studio",
@@ -23,6 +24,13 @@ export default defineConfig({
     structureTool({ structure }),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({ defaultApiVersion: apiVersion })
+    visionTool({ defaultApiVersion: apiVersion }),
+    presentationTool({
+      previewUrl: {
+        previewMode: {
+          enable: "/api/draft-mode/enable"
+        }
+      }
+    })
   ]
 });
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,5 +6,8 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  stega: {
+    studioUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/studio`
+  }
 });
