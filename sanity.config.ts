7bacf113474615b2518356782b1fb73acbeda802
yamanidev <elamine.yamani@gmail.com>
2024-11-20 22:46:38 +0100

    replaced Presentation with Tinloof's Pages wrapper

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,6 +5,7 @@
  */
 
 import { visionTool } from "@sanity/vision";
+import { pages } from "@tinloof/sanity-studio";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 
@@ -12,7 +13,6 @@ import { structureTool } from "sanity/structure";
 import { apiVersion, dataset, projectId } from "./sanity/env";
 import { schema } from "./sanity/schemaTypes";
 import { structure } from "./sanity/structure";
-import { presentationTool } from "sanity/presentation";
 
 export default defineConfig({
   basePath: "/studio",
@@ -21,16 +21,17 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
-    structureTool({ structure }),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({ defaultApiVersion: apiVersion }),
-    presentationTool({
+    pages({
+      creatablePages: ["page"],
       previewUrl: {
         previewMode: {
           enable: "/api/draft-mode/enable"
         }
       }
-    })
+    }),
+    structureTool({ structure }),
+    // Vision is for querying with GROQ from inside the Studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    visionTool({ defaultApiVersion: apiVersion })
   ]
 });
