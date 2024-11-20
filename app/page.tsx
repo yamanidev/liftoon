146177cb011c84d836b09a9761dfd09b4ed0a5e5
yamanidev <elamine.yamani@gmail.com>
2024-11-20 13:06:00 +0100

    increased title font size

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
                   className="h-[37.5rem] rounded-md object-cover"
                   priority
                 />
-                <h2 className="mb-1 mt-2 font-bold">{caseStudy.title}</h2>
+                <h2 className="mb-1 mt-2 text-lg font-bold">{caseStudy.title}</h2>
                 <p className="text-sm text-gray-300">{caseStudy.description}</p>
               </article>
             </Link>
