aace04fa1f3e0c5b49648b363d75c503858644e5
yamanidev <elamine.yamani@gmail.com>
2024-11-20 13:14:29 +0100

    replaced the depracated `domains` property with `remotePatterns`

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["cdn.sanity.io"]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io"
+      }
+    ]
   }
 };
 
