9f70625c6dfb485735bc5a18bcc5c50e86d9194a
yamanidev <elamine.yamani@gmail.com>
2024-11-21 03:51:32 +0100

    added project link to hero section

--- components/layout/CaseStudyHeroSection.tsx
+++ components/layout/CaseStudyHeroSection.tsx
@@ -1,4 +1,5 @@
 import { CaseStudyHero } from "@/sanity.types";
+import Link from "next/link";
 
 export default function CaseStudyHeroSection({ data }: { data: CaseStudyHero }) {
   const title = data.title;
@@ -8,6 +9,15 @@ export default function CaseStudyHeroSection({ data }: { data: CaseStudyHero }) 
     <section className="py-20 text-center sm:py-32">
       <h1 className="text-3xl font-bold">{title}</h1>
       {description && <p className="mt-2 text-gray-300">{description}</p>}
+      {data.project?.projectLink && (
+        <Link
+          href={data.project.projectLink}
+          className="mt-10 inline-block rounded-md bg-white px-4 py-1.5 text-gray-950 transition hover:bg-gray-200"
+          rel="noopener"
+          target="_blank">
+          {data.project.projectText || "Link"}
+        </Link>
+      )}
     </section>
   );
 }
