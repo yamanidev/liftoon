9650af9fe6fd088026c7a5a1c22445f29a0aba82
yamanidev <elamine.yamani@gmail.com>
2024-11-21 03:35:01 +0100

    applied min height to avoid changing heights between pages

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -11,36 +11,34 @@ export default function Navbar() {
 
   return (
     <header className="fixed inset-x-0 bottom-0 sm:bottom-auto sm:top-0">
-      <nav>
-        <div className="mx-auto flex w-full max-w-5xl items-center justify-center px-2 py-4 sm:justify-between">
-          <Link href="/" className="hidden font-bold sm:inline">
-            Liftoon
+      <nav className="mx-auto flex min-h-[4.5rem] w-full max-w-5xl items-center justify-center px-2 py-4 sm:justify-between">
+        <Link href="/" className="hidden font-bold sm:inline">
+          Liftoon
+        </Link>
+        {isCaseStudyPage ? (
+          <Link
+            href="/"
+            className="inline-block rounded-md bg-white px-3 py-1.5 text-gray-950 transition hover:bg-gray-200">
+            Back to home page
           </Link>
-          {isCaseStudyPage ? (
-            <Link
-              href="/"
-              className="inline-block rounded-md bg-white px-3 py-1.5 text-gray-950 transition hover:bg-gray-200">
-              Back to home page
-            </Link>
-          ) : (
-            <div className="flex items-center rounded-md bg-gray-900 p-1">
-              <ul className="flex items-center gap-1 text-sm">
-                <li>
-                  <NavbarLink href="/">Work</NavbarLink>
-                </li>
-                <li>
-                  <NavbarLink href="#">Blog</NavbarLink>
-                </li>
-                <li>
-                  <NavbarLink href="#">Contact</NavbarLink>
-                </li>
-              </ul>
-              <button className="ml-5">
-                <MagnifyingGlassIcon height="24" width="24" />
-              </button>
-            </div>
-          )}
-        </div>
+        ) : (
+          <div className="flex items-center rounded-md bg-gray-900 p-1">
+            <ul className="flex items-center gap-1 text-sm">
+              <li>
+                <NavbarLink href="/">Work</NavbarLink>
+              </li>
+              <li>
+                <NavbarLink href="#">Blog</NavbarLink>
+              </li>
+              <li>
+                <NavbarLink href="#">Contact</NavbarLink>
+              </li>
+            </ul>
+            <button className="ml-5">
+              <MagnifyingGlassIcon height="24" width="24" />
+            </button>
+          </div>
+        )}
       </nav>
     </header>
   );
